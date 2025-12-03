84544a4a8563704e0e8d7875e8b842e914a204e1
Ebenezer Amakato <ebenezeramakato96@gmail.com>
2025-12-03 14:35:24 +0100

    Updated my library

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,3 +1,31 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Book, Library
+from django.views.generic import DetailView
+
 
 # Create your views here.
+def list_books(request):
+    """
+    Function-based view that returns a simple text list
+    of all books and their authors.
+    """
+    books = Book.objects.select_related('author').all()
+
+    # Build a plain-text response
+    lines = []
+    for book in books:
+        lines.append(f"{book.title} by {book.author.name}")
+
+    response_text = "\n".join(lines) or "No books available."
+    return HttpResponse(response_text, content_type="text/plain")
+
+class LibraryDetailView(DetailView):
+    """
+    Class-based view to display details for a specific library,
+    including all books available in that library.
+    """
+    model = Library
+    template_name = "library_detail.html"   # we'll add this template (optional)
+    context_object_name = "library"
+
