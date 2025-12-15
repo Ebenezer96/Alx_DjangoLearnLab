44e7f0956beea531067e52da2cea13554d5318cf
Ebenezer Amakato <ebenezeramakato96@gmail.com>
2025-12-15 07:53:14 +0100

    views has been updated

--- social_media_api/accounts/views.py
+++ social_media_api/accounts/views.py
@@ -3,7 +3,11 @@ from rest_framework.response import Response
 from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework.authtoken.models import Token
 from rest_framework.generics import RetrieveUpdateAPIView
-
+from django.shortcuts import get_object_or_404
+from rest_framework import generics, permissions
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from .models import CustomUser
 from .models import User
 from .serializers import (
     RegisterSerializer,
@@ -38,3 +42,31 @@ class ProfileView(RetrieveUpdateAPIView):
 
     def get_object(self):
         return self.request.user
+
+class FollowUserView(APIView):
+    permission_classes = [permissions.IsAuthenticated]
+
+    def post(self, request, user_id):
+        user_to_follow = get_object_or_404(CustomUser, id=user_id)
+
+        if user_to_follow == request.user:
+            return Response(
+                {"detail": "You cannot follow yourself."},
+                status=400
+            )
+
+        request.user.following.add(user_to_follow)
+        return Response(
+            {"detail": f"You are now following {user_to_follow.username}."}
+        )
+
+class UnfollowUserView(APIView):
+    permission_classes = [permissions.IsAuthenticated]
+
+    def post(self, request, user_id):
+        user_to_unfollow = get_object_or_404(CustomUser, id=user_id)
+
+        request.user.following.remove(user_to_unfollow)
+        return Response(
+            {"detail": f"You unfollowed {user_to_unfollow.username}."}
+        )
--- social_media_api/posts/views.py
+++ social_media_api/posts/views.py
@@ -1,5 +1,4 @@
-from rest_framework import viewsets, permissions, filters
-
+from rest_framework import viewsets, permissions, filters, generics
 from .models import Post, Comment
 from .serializers import PostSerializer, CommentSerializer
 from .permissions import IsAuthorOrReadOnly
@@ -17,6 +16,7 @@ class PostViewSet(viewsets.ModelViewSet):
 
     def perform_create(self, serializer):
         serializer.save(author=self.request.user)
+        
 class CommentViewSet(viewsets.ModelViewSet):
     serializer_class = CommentSerializer
     permission_classes = [
@@ -35,3 +35,14 @@ class CommentViewSet(viewsets.ModelViewSet):
 
     def perform_create(self, serializer):
         serializer.save(author=self.request.user)
+        
+class FeedView(generics.ListAPIView):
+    serializer_class = PostSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        user = self.request.user
+        following_users = user.following.all()
+        return Post.objects.filter(
+            author__in=following_users
+        ).order_by("-created_at")
