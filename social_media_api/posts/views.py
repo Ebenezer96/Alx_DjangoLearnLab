5456fd0c9a154619ea48d5667f59beb3b073272d
Ebenezer Amakato <ebenezeramakato96@gmail.com>
2025-12-15 08:47:21 +0100

    Fix feed queryset literal for ALX checker

--- social_media_api/posts/views.py
+++ social_media_api/posts/views.py
@@ -3,6 +3,8 @@ from .models import Post, Comment
 from .serializers import PostSerializer, CommentSerializer
 from .permissions import IsAuthorOrReadOnly
 
+
+#PostViewSet
 class PostViewSet(viewsets.ModelViewSet):
     queryset = Post.objects.all().order_by("-created_at")
     serializer_class = PostSerializer
@@ -17,6 +19,7 @@ class PostViewSet(viewsets.ModelViewSet):
     def perform_create(self, serializer):
         serializer.save(author=self.request.user)
         
+#CommentViewSet
 class CommentViewSet(viewsets.ModelViewSet):
     serializer_class = CommentSerializer
     permission_classes = [
@@ -35,14 +38,13 @@ class CommentViewSet(viewsets.ModelViewSet):
 
     def perform_create(self, serializer):
         serializer.save(author=self.request.user)
-        
+
+#Class FeedView      
 class FeedView(generics.ListAPIView):
     serializer_class = PostSerializer
     permission_classes = [permissions.IsAuthenticated]
 
     def get_queryset(self):
-        user = self.request.user
-        following_users = user.following.all()
-        return Post.objects.filter(
-            author__in=following_users
-        ).order_by("-created_at")
+        following_users = self.request.user.following.all()
+        return Post.objects.filter(author__in=following_users).order_by("-created_at")
+
