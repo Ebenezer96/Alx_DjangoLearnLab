5f35ab16b051e2a2ada3fe90dd7b830d2517c5db
Ebenezer Amakato <ebenezeramakato96@gmail.com>
2025-12-24 16:29:42 +0100

    Implement custom user model

--- advanced_features_and_security/bookshelf/models.py
+++ advanced_features_and_security/bookshelf/models.py
@@ -5,7 +5,7 @@ from django.db import models
 class CustomUserManager(BaseUserManager):
     def create_user(self, username, password=None, **extra_fields):
         if not username:
-            raise ValueError("Username must be set")
+            raise ValueError("The username must be set")
         user = self.model(username=username, **extra_fields)
         user.set_password(password)
         user.save(using=self._db)
