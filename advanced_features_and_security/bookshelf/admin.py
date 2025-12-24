45a5622796095f6fcbee088d2a48bddaeb45c4a3
Ebenezer Amakato <ebenezeramakato96@gmail.com>
2025-12-24 15:40:16 +0100

    Second commit

--- advanced_features_and_security/bookshelf/admin.py
+++ advanced_features_and_security/bookshelf/admin.py
@@ -1,4 +1,12 @@
 from django.contrib import admin
-from .models import Book
+from django.contrib.auth.admin import UserAdmin
+from .models import CustomUser
 
-admin.site.register(Book)
+
+@admin.register(CustomUser)
+class CustomUserAdmin(UserAdmin):
+    fieldsets = UserAdmin.fieldsets + (
+        ("Additional Fields", {
+            "fields": ("date_of_birth", "profile_photo"),
+        }),
+    )
