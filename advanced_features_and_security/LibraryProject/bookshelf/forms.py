9afd023c80652f6737376cf3135af88dc9623dca
Ebenezer Amakato <ebenezeramakato96@gmail.com>
2025-12-24 19:57:32 +0100

    Add ExampleForm and required templates

--- advanced_features_and_security/LibraryProject/bookshelf/forms.py
+++ advanced_features_and_security/LibraryProject/bookshelf/forms.py
@@ -1,5 +1,7 @@
 from django import forms
 from .models import Book
+from .forms import ExampleForm
+
 
 class BookForm(forms.ModelForm):
     class Meta:
@@ -8,3 +10,7 @@ class BookForm(forms.ModelForm):
 
 class BookSearchForm(forms.Form):
     q = forms.CharField(required=False, max_length=100, strip=True)
+
+class ExampleForm(forms.Form):
+    name = forms.CharField(max_length=100)
+    message = forms.CharField(widget=forms.Textarea)
